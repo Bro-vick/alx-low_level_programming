87bce8f30f3b9f715d246fb08bb0f3df5ac46a0a
George victor <georgev421@gmail.com>
2022-07-14 09:04:51 -0700

    new

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * list_len - returns number of nodes 
+ * list_len - returns number of nodes
  * @h: pointer to linked list
- * Return: number of nodes 
+ * Return: number of nodes
  */
 size_t list_len(const list_t *h)
 {
